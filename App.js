10a57c241fb353be79b7073b9ccf2a65917eb67d
pooja srinivasan <poojaseenu1999@gmail.com>
2022-09-18 00:30:19 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import { Button } from "react-native-web";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>hello world </Text>
+      <TouchableOpacity onPress={() => console.log("image pressed")}>
+        <Image source={{width: 200,height: 300,uri: "https://picsum.photos/200/300"}}/>
+        <Button color='orange' title='click me' onPress={() => alert('button clicked')}></Button>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
